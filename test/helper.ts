427e966b4ca0a479279388ebe509aef2251ec96f
Bartvds <bartvanderschoor@gmail.com>
2013-06-13 22:12:42 +0200

    ditched node fs/path dependencies

--- test/helper.ts
+++ test/helper.ts
@@ -3,22 +3,6 @@
 
 module helper {
 
-	var fs = require('fs');
-	var path = require('path');
-	var util = require('util');
-
-	export function readJSON(...src:string[]):any {
-		return JSON.parse(fs.readFileSync(path.join.apply(path, src)));
-	}
-
-
-	export function dump(object:any, label?:string = '', depth?:number = 6, showHidden?:bool = false):any {
-		if (console.log) {
-			console.log(label + ':');
-		}
-		console.log(util.inspect(object, showHidden, depth, true));
-	}
-
 	export function dumpJSON(object:any, label?:string = ''):any {
 		if (console.log) {
 			console.log(label + ':');
